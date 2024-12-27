20f498d77ad092b6be21b476d85c850ed0918665
Geraldo Barros <gbarrosjr7@gmail.com>
2024-12-27 14:59:52 -0300

    - Adjusted footer year to be dynamic - added policy terms - and linked them in index.html

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,7 @@
-// Função para obter o idioma do navegador do usuário
 function getUserLanguage() {
     return navigator.language || navigator.userLanguage;
 }
 
-// Função para ajustar o conteúdo com base no idioma
 function setLanguageContent(language) {
     if (language === "pt-BR" || language === "pt") {
         document.getElementById("maintenance-title").textContent = "Ops! Estamos em Manutenção";
@@ -14,7 +12,6 @@ function setLanguageContent(language) {
     }
 }
 
-// Função para obter o idioma do usuário com base em sua localização geográfica
 function getUserLanguageByIP() {
     // Substitua "YOUR_IP_GEOLOCATION_API_KEY" pela sua chave de API do serviço de geolocalização
     const apiEndpoint = `https://api.ipgeolocation.io/ipgeo?apiKey=d1fc81d54557431189ba0c07cd7f2cad`;
@@ -32,5 +29,12 @@ function getUserLanguageByIP() {
         });
 }
 
+function updateFooterYear() {
+    const footerText = document.getElementById('footer-text');
+    const currentYear = new Date().getFullYear();
+    footerText.innerHTML = `© ${currentYear} inovartech.com. All rights reserved.`;
+}
+
 
 getUserLanguageByIP();
+updateFooterYear();
